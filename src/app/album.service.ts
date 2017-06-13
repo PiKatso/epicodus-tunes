0f34aa8d751d70a0e2216ca91e56bfdd8f579e85
Kat Gifford and Steven Galvin <steven@>
2017-06-13 16:22:15 -0700

    add firebase and update angular to version 4

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class AlbumService {
+  albums: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) { }
 
   getAlbums() {
     return ALBUMS;
